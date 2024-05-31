27f324aad3524c56ea9b07bf26f279acb8b067e5
Paras Sajwan <parassajwan@Parass-MacBook-Air.local>
2024-05-31 11:28:57 +0530

    updates

--- src/app/story.tsx
+++ src/app/story.tsx
@@ -24,10 +24,16 @@ export default function StoryModal({ data, onClose }: { data: Record<string, any
 
 	return (
 		<div className="story-popup">
+			<div className='story-popup-user'>
+				<div className="story-popup-user-pic">{data.profile_picture && <Image src={data.profile_picture} alt="profile picture" layout='fill' objectFit='contain' /> }</div>
+				<div className='story-popup-user-name'>{data.username}</div>
+			</div>
 			{data.stories && <Image src={data.stories[currentStoryIndex].media_url} alt="story" fill />}
-			<button onClick={onClose}>close</button>
-			<button onClick={prevStory}>prev</button>
-			<button onClick={nextStory}>next</button>
+			<div className='temp-div'>
+				<button onClick={onClose}>close</button>
+				<button onClick={prevStory}>prev</button>
+				<button onClick={nextStory}>next</button>
+			</div>
 		</div>
 	);
 }
